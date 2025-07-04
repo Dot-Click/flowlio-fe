53b182712bf95cecafda7bfdb32f669df41b4194
Dotclick <github@dotclickllc.com>
2025-07-04 12:42:22 +0500

    working on support

--- src/components/support/supportheader.tsx
+++ src/components/support/supportheader.tsx
@@ -1,7 +1,7 @@
 import { ComponentWrapper } from "@/components/common/componentwrapper";
 import { Box } from "@/components/ui/box";
 import { Flex } from "@/components/ui/flex";
-import { PageWrapper } from "../common/pagewrapper";
+
 const SupportHeader = () => {
   return (
     <ComponentWrapper className="mt-6 p-5 shadow-none">
@@ -14,16 +14,17 @@ const SupportHeader = () => {
         </Box>
       </Flex>
 
-      <PageWrapper>
-        <Flex className="justify-between max-md:flex-col max-md:items-start">
-          <Box>
-            <h1 className="text-2xl font-medium capitalize">Support Center</h1>
-            <p className="text-gray-500 mt-1 max-md:text-sm">
-              Need Help? We’ve Got You Covered.
-            </p>
-          </Box>
-        </Flex>
-      </PageWrapper>
+      <Flex className="justify-between max-md:flex-col max-md:items-start bg-white p-5 rounded-lg border border-gray-200 mt-5">
+        <Box>
+          <h1 className="text-md font-medium capitalize">Your Ticket</h1>
+          <p className="text-gray-500 mt-1 max-md:text-sm">
+            You have no ticket yet! Create one by hitting the
+            <span className="underline text-blue-500"> Create Button</span>
+          </p>
+        </Box>
+
+        <p>herebutton</p>
+      </Flex>
     </ComponentWrapper>
   );
 };
